7650ce254b9a894f103d9aa9242137643693b372
matildatrodin <matildatrodin@gmail.com>
2019-05-08 14:10:17 +0200

    Login now works with spotify and connected to our react app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 
+const spotifyApi = new SpotifyWebApi();
+
 class App extends Component {
+    constructor(){
+        super();
+        const params = this.getHashParams();
+        const token = params.access_token;
+        if (token) {
+            spotifyApi.setAccessToken(token);
+        }
+        this.state = {
+            loggedIn: token ? true : false,
+            nowPlaying: { name: 'Not Checked', albumArt: '' }
+        }
+    }
+    getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+            e = r.exec(q);
+        }
+        return hashParams;
+    }
+
+    getNowPlaying(){
+        spotifyApi.getMyCurrentPlaybackState()
+            .then((response) => {
+                this.setState({
+                    nowPlaying: {
+                        name: response.item.name,
+                        albumArt: response.item.album.images[0].url
+                    }
+                });
+            })
+    }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <a href='http://localhost:8888'> Login to Spotify </a>
+          <div>
+              Now Playing: { this.state.nowPlaying.name }
+          </div>
+          <div>
+              <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/>
+          </div>
       </div>
     );
   }
