254d1142173a659dfd4e26822d84e7662661dbfb
sergiocast-97 <sergiocast97@gmail.com>
2019-05-08 14:18:26 +0200

    setup delete

---
+++ src/buttons.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class loginButton extends Component{
+    
+    render() {
+        return (
+            <div className="loginButton">
+                <p>Login</p>
+            </div>
+        );
+    }
+}
