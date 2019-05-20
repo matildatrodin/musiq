22c94179f4ce803f4fef225ac8b8e92c6e70563e
matildatrodin <matildatrodin@gmail.com>
2019-05-20 16:38:56 +0200

    Removed some unecessary code and func generateQuiz now works

---
+++ src/modules/game.js
@@ -0,0 +1,23 @@
+import react, {Component} from 'react';
+import '../index.css';
+
+
+class Game extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            questions: [],
+            answers: [],
+            tracks: []
+        }
+    }
+
+
+    render(){
+        return("");
+    }
+}
+
+export default Game;
